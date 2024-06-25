87c5ad8036a68b659cf6ff25838eda6ef55886ff
kuri884 <krc.kuribayashi@gmail.com>
2024-06-25 21:51:06 +0900

    appElement

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import Modal from "react-modal";
 import styled from "styled-components";
 import Button from "@mui/material/Button";
 import ErrorRoundedIcon from "@mui/icons-material/ErrorRounded";
+
+Modal.setAppElement("App");
 
 const StyledModal = styled(Modal)`
   position: relative;
@@ -50,16 +54,24 @@ const ErrorIcon = <ErrorRoundedIcon style={{ fontSize: "3.5rem" }} color="error"
 
 const DefaultModal: React.FC<AlertProps> = ({ closeModal, modalIsOpen, modalMessage }) => {
   return (
-    <StyledModal isOpen={modalIsOpen} onRequestClose={closeModal} ariaHideApp={false} contentLabel="AlertModal">
-      <Title>
-        {ErrorIcon}
-        <ErrorMessage id="modal-title">警告</ErrorMessage>
-      </Title>
-      <ModalMessage>{modalMessage}</ModalMessage>
-      <OKButton id="okButton" variant="contained" color="primary" onClick={closeModal}>
-        OK
-      </OKButton>
-    </StyledModal>
+    <div id="App">
+      <StyledModal
+        //   appElement={document.getElementById("root") as HTMLElement}
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        //   ariaHideApp={false}
+        contentLabel="AlertModal"
+      >
+        <Title>
+          {ErrorIcon}
+          <ErrorMessage id="modal-title">警告</ErrorMessage>
+        </Title>
+        <ModalMessage>{modalMessage}</ModalMessage>
+        <OKButton id="okButton" variant="contained" color="primary" onClick={closeModal}>
+          OK
+        </OKButton>
+      </StyledModal>
+    </div>
   );
 };
 
