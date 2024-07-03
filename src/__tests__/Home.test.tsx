2216992d204491f96b760a9201430cb53459c1e4
kuri884 <krc.kuribayashi@gmail.com>
2024-07-03 10:24:13 +0900

    componentテストを追加

--- src/__tests__/Home.test.tsx
+++ src/__tests__/Home.test.tsx
@@ -1,10 +1,15 @@
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Home from "../app/page";
 import React from "react";
 
 describe("ホームのコンポーネントテスト", () => {
   test("コンポーネントテスト", () => {
     render(<Home />);
+
+    expect(screen.getByText("Button")).toBeInTheDocument();
+    expect(screen.getByText("Checkbox")).toBeInTheDocument();
+    expect(screen.getByText("Modal")).toBeInTheDocument();
+    expect(screen.getByText("Tab")).toBeInTheDocument();
   });
 });
