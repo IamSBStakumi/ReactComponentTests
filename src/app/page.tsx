b03a3acde3f296940b31b38a484317dc4146d67c
kuri884 <krc.kuribayashi@gmail.com>
2024-07-03 10:22:18 +0900

    ホームから各ページへ飛べるようにした

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
+"use client";
+
+import Link from "next/link";
+import styled from "styled-components";
+
+const LinkText = styled(Link)`
+  font-size: 32px;
+  padding-left: 1.5rem;
+  display: block;
+`;
+
 export default function Home() {
   return (
     <main>
-      <div></div>
+      <div>
+        <h1>Components</h1>
+        <LinkText href="/buttons">Button</LinkText>
+        <LinkText href="/checkbox">Checkbox</LinkText>
+        <LinkText href="/modal">Modal</LinkText>
+        <LinkText href="/tab">Tab</LinkText>
+      </div>
     </main>
   );
 }
